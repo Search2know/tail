c303c4357b042b881851363c3961b54dfe2d22e0
Search2know <kovlachishin.kb@mail.ru>
2019-04-25 23:31:58 +0300

    update

--- src/main/java/tail.java
+++ src/main/java/tail.java
@@ -7,9 +7,10 @@ import java.io.*;
 import java.util.ArrayList;
 
 
+
 public class tail {
-        @Option(name = "-c", usage = "last chars in file", forbids = {"-n"})
-        private int chars = -1;
+    @Option(name = "-c", usage = "last chars in file", forbids = {"-n"})
+    private int chars = -1;
 
     @Option(name = "-n", usage = "last strings in file", forbids = {"-c"})
     private int files = -1;
@@ -25,7 +26,7 @@ public class tail {
         new tail().start(args);
     }
 
-    public void start(String[] args) throws IOException {
+    private void start(String[] args) throws IOException {
         CmdLineParser parse = new CmdLineParser(this);
         try {
             parse.parseArgument(args);
@@ -34,55 +35,55 @@ public class tail {
             System.err.println("java -jar tail.jar  -c|-n num -o basicOutputName inputFileName1 inputFileName2 ...");
             parse.printUsage(System.err);
         }
-        if (input.size() == 1) {
+        if (input.size() != 0) {
             if (chars != -1)
-                tail.splitterC(input, chars, output);
+                splitterC(input, chars, output);
             else if (files != -1)
-                tail.splitterN(input, files, output);
+                splitterN(input, files, output);
 
         }
+
     }
 
-    public static void splitterC(ArrayList<String> inputter, int count, String output) throws IOException {
+
+    private static void splitterC(ArrayList<String> inputter, int count, String output) throws IOException {
         Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(output, true), "UTF-8"));
-        for (int i = 0; i <= inputter.size() - 1; i++) {
-        BufferedReader buf = new BufferedReader(new FileReader(new File(inputter.get(i))));
-        int charser = 0;
-        ArrayList<Character> list0fList = new ArrayList<Character>();
-        do {
-            charser = buf.read();
-            list0fList.add((char) charser);
-        }
-        while (charser != -1);
-        for (int j = list0fList.size() - 1 - count;j < list0fList.size() - 1; j++) {
+                new FileOutputStream(output, false)));
+        for (String anInputter : inputter) {
+            BufferedReader buf = new BufferedReader(new FileReader(new File(anInputter)));
+            int charser;
+            ArrayList<Character> list0fList = new ArrayList<Character>();
+            do {
+                charser = buf.read();
+                list0fList.add((char) charser);
+            }
+            while (charser != -1);
+            for (int j = list0fList.size() - 1 - count; j < list0fList.size() - 1; j++) {
 
-         writer.write((char) charser);
-
-        }
-
+                writer.write(list0fList.get(j));
+            }
         }
         writer.close();
     }
 
-    public static void splitterN(ArrayList<String> inputter, int count, String output) throws IOException {
-        for (int i = 0; i <= inputter.size() - 1; i++) {
-        BufferedReader buf = new BufferedReader(new FileReader(new File(inputter.get(i))));
-        String liner;
-        ArrayList<String> list0fList = new ArrayList<String>();
-        do {
-            liner = buf.readLine();
-            list0fList.add(liner);
-        }
-        while (liner != null);
-        OutputStream os = null;
-        for (int j = list0fList.size() - 1 - count; j < list0fList.size() - 1; j++) {
+    private static void splitterN(ArrayList<String> inputter, int count, String output) throws IOException {
+        Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(output, false)));
+        for (String anInputter : inputter) {
+            BufferedReader buf = new BufferedReader(new FileReader(new File(anInputter)));
+            String liner;
+            ArrayList<String> list0fList = new ArrayList<String>();
+            do {
+                liner = buf.readLine();
+                list0fList.add(liner);
+            }
+            while (liner != null);
+            for (int j = list0fList.size() - 1 - count; j < list0fList.size() - 1; j++) {
 
-            os = new FileOutputStream(new File(output), true);
-            os.write(list0fList.get(j).getBytes(), 0, list0fList.get(j).length());
+                writer.write(list0fList.get(j));
+            }
         }
-            os.close();
-    }
+        writer.close();
     }
 
 }
